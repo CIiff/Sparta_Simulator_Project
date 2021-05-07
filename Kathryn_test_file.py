646114706167429707f445fc9dd938380f2d3f53
Kathryn <kdonnell997@hotmail.co.uk>
2021-05-07 14:39:48 +0100

    trainee generator

--- Kathryn_test_file.py
+++ Kathryn_test_file.py
@@ -4,6 +4,8 @@
 # number of full centres
 # number of trainees currently training
 # number of trainees on the waiting list
+import random
+
 
 class SpartaSimulation:
 
@@ -11,8 +13,15 @@ class SpartaSimulation:
         self.month = month
         self.num_open_centres = 0
         self.num_full_centres = 0
-        self.num_current_trainees = 0
+        self.num_current_trainees = self.trainee_generator()
         self.num_waiting_list = 0
+
+    def trainee_generator(self):
+        total_trainees = 0
+        for month in range(self.month):
+            new_trainees = random.randint(20, 30)
+            total_trainees += new_trainees
+        return total_trainees
 
     def get_num_open_centres(self):
         return self.num_open_centres
@@ -26,3 +35,10 @@ class SpartaSimulation:
     def get_num_waiting_list(self):
         return self.num_waiting_list
 
+# AS A user
+# I WANT a method to generate random trainees
+# SO THAT they can be added to courses every month
+# between 20 and 30
+
+
+print(SpartaSimulation(2).get_num_current_trainees())
