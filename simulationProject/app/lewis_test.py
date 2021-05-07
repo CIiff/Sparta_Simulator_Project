cbc76e42cff11109063817a03ce9a648ed965361
Lewis <ltwelftree@spartaglobal.com>
2021-05-07 12:15:07 +0100

    get test func implemented

---
+++ simulationProject/app/lewis_test.py
@@ -0,0 +1,21 @@
+import unittest
+from simulationProject.app.spartasim import SpartaSimulation
+
+class GetTests(unittest.TestCase):
+
+    test_sim_instance = SpartaSimulation()
+
+    def test_get_num_open_centres(self):
+        self.assertEqual(self.test_sim_instance.get_num_open_centres(), )
+
+    def test_get_num_full_centres(self):
+        self.assertEqual(self.test_sim_instance.test_get_num_full_centres(), )
+
+    def test_get_num_current_trainees(self):
+        self.assertEqual(self.test_sim_instance.test_get_num_current_trainees(), )
+
+    def test_get_num_waiting_list(self):
+        self.assertEqual(self.test_sim_instance.test_get_num_waiting_list(), )
+        self.assert
+
+
