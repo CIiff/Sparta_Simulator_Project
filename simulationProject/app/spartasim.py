4818431a889282f717f6618612f725df36fdf50d
Lewis Twelftree <37084566+Infuro@users.noreply.github.com>
2021-05-07 16:08:47 +0100

    Update spartasim.py
    made some changes to trainee generation and others

--- simulationProject/app/spartasim.py
+++ simulationProject/app/spartasim.py
@@ -7,21 +7,17 @@ class SpartaSimulation:
     def __init__(self, month):
         self.month = month
         self.current_month = 1
-        self.num_open_centres = 0
+        self.num_open_centres = 1
         self.num_full_centres = 0
-        self.num_current_trainees = self.trainee_generator()
+        self.num_current_trainees = 0
         self.num_waiting_list = 0
-        self.centers = self.create_center()
+        self.centers = {1:0}
 
     def month_inc(self):
         self.current_month += 1
 
     def trainee_generator(self):
-        total_trainees = 0
-        for month in range(self.month):
-            new_trainees = random.randint(20, 30)
-            total_trainees += new_trainees
-        return total_trainees
+        self.num_current_trainees = random.randint(20, 30)
 
     def get_num_open_centres(self):
         return self.num_open_centres
@@ -34,9 +30,6 @@ class SpartaSimulation:
 
     def get_num_waiting_list(self):
         return self.num_waiting_list
-
-    def create_center(self):
-        return {1:0}
 
     def add_new_center(self):
         newcenter_id = len(self.centers.keys()) + 1
