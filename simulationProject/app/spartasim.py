27338ecc0f7a973a6bb8da1860ee10a44c27bb64
Kathryn <kdonnell997@hotmail.co.uk>
2021-05-12 15:01:11 +0100

    removing get methods

--- simulationProject/app/spartasim.py
+++ simulationProject/app/spartasim.py
@@ -41,18 +41,6 @@ class SpartaSimulation:
                   loc=new_train_mean, scale=new_train_stdev, size=1))
         return round(num_new_trainees)
 
-    def get_num_open_centres(self):
-        return self.num_open_centres
-
-    def get_num_full_centres(self):
-        return self.num_full_centres
-
-    def get_num_current_trainees(self):
-        return self.trainees_in_training
-
-    def get_num_waiting_list(self):
-        return self.num_waiting_list
-
     def add_new_center(self):
         new_center_id = len(self.centers.keys()) + 1
         self.num_open_centres += 1
@@ -74,10 +62,6 @@ class SpartaSimulation:
             self.assign_trainees_to_center()
             self.count_full_centers()
             self.month_inc()
-
-    def count_full_centers(self):
-        # Checks for each center if they're at max capacity yet.
-        self.num_full_centres = sum(value == self.centre_max_capacity for value in self.centers.values())
 
     def assign_trainee_to_course(self):
         num_new_trainees = self.trainee_generator()
