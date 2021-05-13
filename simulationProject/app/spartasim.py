62c503fb2d4ebcda3bb74e2b3d4e8259cbfdd45b
Lewis <ltwelftree@spartaglobal.com>
2021-05-13 12:21:31 +0100

    commit

--- simulationProject/app/spartasim.py
+++ simulationProject/app/spartasim.py
@@ -4,12 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 import csv
-import pandas as pd
-import scipy.stats as stats
+from tabulate import tabulate
+
 
 class SpartaSimulation:
 
-    def __init__(self, months_to_simulate, min_hired_trainees, max_hired_trainees, centre_size=100):
+    def __init__(self, months_to_simulate=120, min_hired_trainees=20, max_hired_trainees=30):
         self.stopping_month = months_to_simulate + 1
         self.current_month = 1
         self.num_waiting_list = 0
@@ -19,11 +19,8 @@ class SpartaSimulation:
         self.courses = ["Data", "DevOps", "C#", "Java", "Business"]
         self.status_list = ["Waiting", "Training", "Benched"]
         self.centres_df = pd.DataFrame(columns=['Centre type', 'Trainee count', 'Max capacity','Low att month counter', 'Centre course type','Centre status'])
-        #self.centre_types = {'Boot camp': 0, 'Hub': 0, 'Tech': {'Java':0,'C#':0,'Data':0,'DevOps':0,'Business':0}}
         self.available_centre_types = ['Boot camp', 'Hub', 'Tech centre']
-        self.available_tech_centre_types = ['Java','C#','Data','DevOps','Business']
-        #self.simulation_loop()
-
+        self.simulation_loop()
 
     def month_inc(self):
         self.current_month += 1
@@ -43,54 +40,56 @@ class SpartaSimulation:
     def create_centre(self):
         self.count_centres()
         chosen_centre_type = random.choice(self.available_centre_types)
-        tech_centre_type = random.choice(self.available_tech_centre_types)
+        tech_centre_type = random.choice(self.courses)
 
-        hub_template = [{'Centre type': 'Hub', 'Trainee count': 0, 'Max capacity': 100, 'Low att month counter': 0,'Centre course type': 'None', 'Centre status': 'open'}]
-        boot_camp_template = [{'Centre type': 'Boot camp', 'Trainee count': 0, 'Max capacity': 500, 'Low att month counter': 0,'Centre course type': 'None', 'Centre status': 'open'}]
-        tech_centre_template = [{'Centre type': 'Tech centre', 'Trainee count': 0, 'Max capacity': 200, 'Low att month counter': 0,'Centre course type': f'{tech_centre_type}', 'Centre status': 'open'}]
+        hub_template = [{'Centre type': 'Hub', 'Trainee count': 0, 'Max capacity': 100, 'Low att month counter': 0,'Centre course type': 'None', 'Centre status': 'Open'}]
+        boot_camp_template = [{'Centre type': 'Boot camp', 'Trainee count': 0, 'Max capacity': 500, 'Low att month counter': 0,'Centre course type': 'None', 'Centre status': 'Open'}]
+        tech_centre_template = [{'Centre type': 'Tech centre', 'Trainee count': 0, 'Max capacity': 200, 'Low att month counter': 0,'Centre course type': f'{tech_centre_type}', 'Centre status': 'Open'}]
 
         if chosen_centre_type == 'Hub':
             self.centres_df = self.centres_df.append(hub_template, ignore_index=True)
-        if chosen_centre_type == 'Boot camp':
+        elif chosen_centre_type == 'Boot camp':
             self.centres_df = self.centres_df.append(boot_camp_template, ignore_index=True)
-        if chosen_centre_type == 'Tech centre':
+        elif chosen_centre_type == 'Tech centre':
             self.centres_df = self.centres_df.append(tech_centre_template, ignore_index=True)
 
+        self.centre_debug_prints(chosen_centre_type)
 
     def count_centres(self):
-        counted_tech_centre_types = dict(Counter(self.centres_df['Centre course type']))
-        print(counted_tech_centre_types.items())
+        bootcamp = self.centres_df.loc[
+            (self.centres_df['Centre status'] == "Open") &
+            (self.centres_df['Centre type'] == 'Boot camp')].count()[0]
 
-        for k, v in counted_tech_centre_types.items():
-            if k in self.available_tech_centre_types and v >= 1:
-                self.available_tech_centre_types.remove(k)
-
-        counted_centre_types = dict(Counter(self.centres_df['Centre type']))
+        hub = self.centres_df.loc[
+            (self.centres_df['Centre status'] == "Open") &
+            (self.centres_df['Centre type'] == 'Hub')].count()[0]
 
         # pop hub from available centre types
-        if counted_centre_types['Hub'] >= 3:
-            self.available_centre_types.remove('Hub')
-        if 'Hub' not in self.available_centre_types and counted_centre_types['Hub'] < 3:
+        if 'Hub' in self.available_centre_types:
+            if hub >= 3:
+                self.available_centre_types.remove('Hub')
+        if 'Hub' not in self.available_centre_types and hub < 3:
             self.available_centre_types.append('Hub')
 
         #pop boot camp from available centre types
-        if counted_centre_types['Boot camp'] >= 2:
-            self.available_centre_types.remove('Boot camp')
-        if 'Boot camp' not in self.available_centre_types and counted_centre_types['Boot camp'] < 2:
+        if 'Boot camp' in self.available_centre_types:
+            if bootcamp >= 2:
+                self.available_centre_types.remove('Boot camp')
+        if 'Boot camp' not in self.available_centre_types and bootcamp < 2:
             self.available_centre_types.append('Boot camp')
-
-        # pop tech centre from available centre types
-        if self.available_tech_centre_types == []:
-            self.available_centre_types.remove('Tech centre')
-        if not self.available_tech_centre_types == []:
-            self.available_centre_types.append('Tech centre')
 
         #prioritise filling centres with less than 25 and tech centres, the fill hub and boot camp
 
+    def centre_debug_prints(self, chosen_centre_type):
+        print('MONTH:', self.current_month, '/ ', self.available_centre_types)
+        print('CHOSEN: ', chosen_centre_type)
+        print(tabulate(self.centres_df))
+        print('////////////////////')
+
     def simulation_loop(self):
         while self.current_month <= self.stopping_month:
-            if self.current_month % 2 == 1 and self.current_month != 1:
-                self.add_new_center()
+            if self.current_month % 2 == 1:
+                self.create_centre()
             self.trainee_generator()
             self.month_inc()
 
