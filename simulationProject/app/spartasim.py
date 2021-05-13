9079afb20251c12c80eaedc61e168eb5c24e3e57
AlexlwAstro <alexander.lw@me.com>
2021-05-13 12:19:14 +0100

    small changes, wanna checkout to main, so commiting

--- simulationProject/app/spartasim.py
+++ simulationProject/app/spartasim.py
@@ -1,11 +1,9 @@
 import random
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from collections import Counter
-import csv
 import pandas as pd
 import scipy.stats as stats
+import logging
 
 
 class SpartaSimulation:
